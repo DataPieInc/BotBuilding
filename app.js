dc096d4184b482b964848ea6179873b60975dd83
Dennis Masesi <datapieinc@gmail.com>
2016-10-24 11:06:49 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+//Setting up the environment
+var builder = require('botbuilder');
+var restify = require('restify');
+
+var connector = new builder.ChatConnector();
+var bot = new builder.UniversalBot(connector);
+var intents = new builder.IntentDialog();
+// Dialog
+// bot.dialog('/',function(session)
+// {
+//    session.send('Hello World');
+// });
+// Dialog
+bot.dialog('/', intents);
+
+intents.matches(/^change name/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
+
+intents.onDefault([
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
+
+// Create a server to communicate with api/messages
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+    console.log('%s listening to %s', server.name, server.url);
+});
+server.post('/api/messages', connector.listen());
